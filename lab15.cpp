6019e91602ef85a002214e10395bdc1bd52288c4
Joey <imjoeydhillon@gmail.com>
2024-09-29 20:27:32 -0700

    second commit

--- lab15.cpp
+++ lab15.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
+using namespace std;
+
+class Movie
+{
+private:
+    string screenWriter;
+    string title;
+    int year;
+    
+public:
+    Movie(string screenWriter, string title, int year) : screenWriter{screenWriter} ;
+    ~Movie();
+};
+
 int main(int argc, char const *argv[])
 {
+
     return 0;
 }
